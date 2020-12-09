94010eacf5320dc591f221a53157690c4df216d0
Tavs64 <73593582+Tavs64@users.noreply.github.com>
2020-12-09 13:53:51 +0100

    Update TkinterGUI.py

--- TkinterGUI.py
+++ TkinterGUI.py
@@ -1,20 +1,23 @@
 from tkinter import *
 import sys
 
+
 class Application(Frame):  # Application is a Frame (inheritance from Frame)
     def __init__(self, master):
         Frame.__init__(self, master) 
         self.grid(sticky=N+S+E+W) # put frame in toplevel window
+        #self.createBackground()
         self.createWidgets()
 
     def commandHandler(self, bNo):
         print("Cmd handler called: " + str(bNo))
 
-    def createBackground():
-        Imagelocation = Image.open("C:\\Users\\tavss\\OneDrive\\Dokumenter\\GitHub\\Tkinter-Gui-Andegradsligning\\Gradient.png")
-        
-        
-    
+    def createBackground(self):
+        #Gradient = PhotoImage(file = "C:\\Users\\tavss\\OneDrive\\Dokumenter\\GitHub\\Tkinter-Gui-Andegradsligning\\Gradient.png")
+        background_label = Label(top, image=Gradient)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+
     def createWidgets(self):
         top=self.winfo_toplevel()
         #top.geometry("500x500")
@@ -26,17 +29,18 @@ class Application(Frame):  # Application is a Frame (inheritance from Frame)
             self.rowconfigure(row, weight=1 , minsize=100)
 
         # columns with different scale
-        for i in range(0,3):
-            self.columnconfigure(i, weight=i, minsize=200)
+        for i in range(0,4):
+            self.columnconfigure(i, weight=i, minsize=300)
 
         # create 2 labels
-        colors = ('yellow', 'green')    
-        for row in range(0,2):
-            l=Label(self, text=f'row:{row}', justify="left", bg=colors[row])
+        colors = ('#FA4EB2', '#DB3BE3', '#C142FD')    
+        letter = ('A','B','C')
+        for row in range(0,3):
+            l=Label(self, text=letter[row] + ' value', justify="left", bg=colors[row])
             l.grid(row=row, column=0, sticky=S+W+N+E)
 
         # create 3 buttons
-        for i in range(0,3):
+        for i in range(1,4):
             self.columnconfigure(i, weight=i) # , minsize=200
             def cmd(no=i):  # hidden argument trick
                 self.commandHandler(no)  
